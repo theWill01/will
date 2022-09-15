255cddc18c2e250929dbc471720d82849f8d6fb4
will <thewilldev01@gmail.com>
2022-09-15 21:51:44 +0100

    will

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Nav from "./components/Nav";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Wave from "./assets/images/wave(1).svg";
+import Grass from "./assets/images/wav3.svg";
+import Bird from "./assets/images/bird.gif";
 
 function App() {
+  const [element, setElement] = useState(false);
 
-
-
-
+  useEffect(() => {
+    setElement(true);
+  }, []);
 
   return (
     <div className="App">
@@ -20,18 +24,17 @@ function App() {
           <div className="anchor1">
             <Link to="#" className="anchors">
               <span className="git">
-                <h3 > GitHub</h3>
+                <h3> GitHub</h3>
               </span>
             </Link>
             <Link to="#" className="anchors">
               <span className="linkedIn">
-           
                 <h3>Linked-in</h3>
               </span>
             </Link>
           </div>
 
-          <div className="container">
+          <section className="container">
             <div className="anchor2 flex justify-between">
               <Link to="/">
                 <h3>Will Albert</h3>
@@ -49,16 +52,148 @@ function App() {
                 <p>Developer & Designer</p>
               </div>
               <div className="display">
-                <Routes>
-                  <Route path="/" element={<About />} />
+                <div className="canvas">
+                  <div className="birds">
+                    <img src={Bird} alt="#" />
+                    <img src={Bird} alt="#" />
+                    <img src={Bird} alt="#" />
+                    <img src={Bird} alt="#" />
+                    <img src={Bird} alt="#" />
+                    <img src={Bird} alt="#" />
+                  </div>
+                  <div className={element ? "true" : "false"}></div>
+                  <div className="mountain">
+                    <div className="hill">
+                      <div className="hillGrass1">
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                      </div>
+                      <div className="hillWater">
+                        <div className="river">
+                          <div className="flow"></div>
+                          <div className="flow"></div>
+                          <div className="flow"></div>
+                          <div className="flow"></div>
+                          <div className="flow"></div>
+                          <div className="flow"></div>
+                        </div>
+                        <div className="splash">
+                          <div className="drops"></div>
+                          <div className="drops"></div>
+                          <div className="drops"></div>
+                          <div className="drops"></div>
+                          <div className="drops"></div>
+                        </div>
+                        <div className="waves">
+                          <div className="wave1">
+                            <div></div>
+                            <div></div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="hillGrass2">
+                        {" "}
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                      </div>
+                    </div>
+                    <div className="valley">
+                      <div className="valleyGrass1">
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                      </div>
+                      <div className="valleyWater">
+                        <div className="rush">
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                          <hr />
+                        </div>{" "}
+                        <div className="vw">
+                          <div className="flow"></div>
+                          <div className="flow"></div>
+                          <div className="flow"></div>
+                          <div className="flow"></div>
+                          <div className="flow"></div>
+                          <div className="flow"></div>
+                        </div>
+                        <div className="vsplash">
+                          <div className="drops"></div>
+                          <div className="drops"></div>
+                          <div className="drops"></div>
+                          <div className="drops"></div>
+                          <div className="drops"></div>
+                        </div>
+                      </div>
+                      <div className="valleyGrass2">
+                        <img src={Wave} alt="#" />
+                        <img src={Wave} alt="#" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="cover">
+                  <Routes>
+                    <Route path="/" element={<About />} />
 
-                  <Route path="/projects" element={<Projects />} />
+                    <Route path="/projects" element={<Projects />} />
 
-                  <Route path="/contact" element={<Contact />} />
-                </Routes>
+                    <Route path="/contact" element={<Contact />} />
+                  </Routes>
+                </div>
               </div>
             </div>
-          </div>
+          </section>
         </section>
       </main>
     </div>
